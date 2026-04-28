f2f081693718046cb102b3ba3641d4dcd60f3bee
unknown <vikenzo.me@gmail.com>
2026-04-28 08:40:25 +0500

    Move Django admin to /django-admin/, add teacher type to edit form

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django-admin/', admin.site.urls),
     path('api/auth/', include('apps.users.urls')),
     path('api/students/', include('apps.students.urls')),
     path('api/groups/', include('apps.groups.urls')),
